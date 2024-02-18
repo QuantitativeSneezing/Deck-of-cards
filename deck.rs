f8907369969c55f02232a5be6cbdda64132ff159
Jason Arnold <jatsburgern@gmail.com>
2024-02-18 14:38:24 -0500

    trying druid, init card struct

--- deck.rs
+++
@@ -1,3 +0,0 @@
-struct deck{
-    cards: Vec<String>
-}
---
+++ src/deck.rs
@@ -0,0 +1,6 @@
+use druid::{AppLauncher, WindowDesc};
+Struct Card{
+    suit: String,
+    value: String
+}
+let mut deck :Vec<Card> = Vec::new();
