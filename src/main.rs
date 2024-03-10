54d1c95c3108e0ac1a8f407358f8fe870fafee40
Jason Arnold <jatsburgern@gmail.com>
2024-03-10 00:02:20 -0500

    try egui instead

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+// ui.heading("My egui Application");
+// ui.horizontal(|ui| {
+//     ui.label("Your name: ");
+//     ui.text_edit_singleline(&mut name);
+// });
+// ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+// if ui.button("Increment").clicked() {
+//     age += 1;
+// }
+// ui.label(format!("Hello '{name}', age {age}"));
+// ui.image(egui::include_image!("ferris.png"));
