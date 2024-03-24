903073c71d640be6cc898902729e7f2cb07333e9
Jason Arnold <jatsburgern@gmail.com>
2024-03-24 15:57:40 -0400

    populating deck with cards struct instead of strings lol

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,65 @@
 #[derive(Clone)]
 struct Card {
     suit: String,
-    value: String
+    value: String,
 }
-impl Card{
-    fn read(&self)->String{
-        return format!("{} of {}", self.value, self.suit)
+impl Card {
+    fn read(&self) -> String {
+        return format!("{} of {}", self.value, self.suit);
     }
 }
 struct Deck {
-  cards:Vec<Card>
+    cards: Vec<Card>,
 }
-impl Deck{
-    fn shuffle(&self){
+impl Deck {
+    fn shuffle(&self) {
         println!("shuffling!");
-        let card= &self.cards[0];
-        println!("{} is on top",card.read());
-      }
+        let card = &self.cards[0];
+        println!("{} is on top", card.read());
+    }
+    // fn populate(&self) {
+    //     let suits: Vec<String> = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    //         .map(String::from)
+    //         .to_vec();
+    //     let values: Vec<String> = [
+    //         "Ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack",
+    //         "Queen", "King",
+    //     ]
+    //     .map(String::from)
+    //     .to_vec();
+    //     let mut cards = Vec::new();
+    //     for suit in suits.iter() {
+    //         for value in values.iter() {
+    //             let card = format!("{} of {}", suit, value);
+    //             // println!("{}",card);
+    //             cards.push(card)
+    //         }
+    //     }
+    //     self.cards=cards
+    // }
 }
 fn main() {
-    let card= Card{
-        suit:"spades".to_string(),
-        value:"ace".to_string()
-    };
-    let suits: Vec<String> = ["Spades", "Clubs", "Hearts", "Diamonds"].map(String::from).to_vec();
-    let values: Vec<String> = ["Ace", "two", "three", "four","five","six",
-    "seven","eight","nine","ten", "Jack", "Queen", "King"].map(String::from).to_vec();
+    // let card= Card{
+    //     suit:"spades".to_string(),
+    //     value:"ace".to_string()
+    // };
+    let suits: Vec<String> = ["Spades", "Clubs", "Hearts", "Diamonds"]
+        .map(String::from)
+        .to_vec();
+    let values: Vec<String> = [
+        "Ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack",
+        "Queen", "King",
+    ]
+    .map(String::from)
+    .to_vec();
     let mut cards_test = Vec::new();
-    for suit in suits.iter(){
-        for value in values.iter(){
-            let card= format!("{} of {}", suit, value);
+    for suit_name in suits.iter() {
+        for value_name in values.iter() {
+            let card= Card{suit:suit_name.to_string(), value:value_name.to_string()};
             // println!("{}",card);
-            cards_test.push(card)
+            cards_test.push(card);
         }
     }
-    let deck= Deck{
-        cards:cards_test
-    };
+    let deck = Deck{cards:cards_test};
     deck.shuffle()
 }
